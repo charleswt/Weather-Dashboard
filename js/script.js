d173da3e18d1aac33c8cfc685725cafb881c38e7
ct <charleswtiffany@gmail.com>
2024-01-02 16:39:30 -0800

    started grabbing data from api figuring out how I want to go about doing it the way that I am doing it now seems like it could have issues if something were to change the way the weather was listed in the list array

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 const apiKey = "3b20066a45c23f22eadffa2c8929e33f";
-let city = "Encinitas";
 
-const weatherApi = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+$("#searchBtn").on('click', function () {
+  const input = document.getElementById("input");
+  const city = input.value;
+  const weatherApi = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
-fetch(weatherApi)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+  fetch(weatherApi).then(function (response) {
+    if (response.ok) {
+        response.json().then(function (data) {
+            const weather = data
+            console.log(data);
+            console.log(data.city.name, data.list[0].dt_txt)
+        })
+    }
+    }
+  )
 
+
+    
+
+    
+
+});
+
+// data.city.name 
+// data.list[0].dt_txt 
+// data.list[0].weather.icon 
+// data.list[0].main.humidity 
+// data.list[0].wind.speed 
+// data.list[0].main.temp 
+// 0 4 12 20 28 36
+
+//  idea is that I run a for loop looking for the fist item in the data array that is 1 day later skipping the next times in the same day to the next day skipping and so and so forwarth
